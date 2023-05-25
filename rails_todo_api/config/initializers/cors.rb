4b2ce6871150cb75e2e94588afd95221dd931437
Corbyn Greenwood <corbyn.greenwood.98@gmail.com>
2023-05-25 18:45:48 -0400

    Another attempt so I can pull all my hair out apparently

--- rails_todo_api/config/initializers/cors.rb
+++ rails_todo_api/config/initializers/cors.rb
@@ -1,6 +1,16 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'
+      origins /\A(http[s]{0,1}:\/\/){0,1}(rails\-todo\.corbyngreenwood\.com){1}(\/){0,1}\z/mi
+      resource '/auth',
+        headers: :any,
+        methods: [:post, :options]
+      resource '/todo',
+        headers: :any,
+        methods: [:get, :post, :delete, :options]
+    end
+
+    allow do
+      origins '::1', '127.0.0.1'
       resource '/auth',
         headers: :any,
         methods: [:post, :options]

6a061f5aae8749e441e7737b5d00fb8016b278b1
Corbyn Greenwood <corbyn.greenwood.98@gmail.com>
2023-05-25 19:17:07 -0400

    Final attempt with CORS before I give up for good. I am done with this damn config

--- rails_todo_api/config/initializers/cors.rb
+++ rails_todo_api/config/initializers/cors.rb
@@ -9,13 +9,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         methods: [:get, :post, :delete, :options]
     end
 
-    allow do
-      origins '*'
-      resource '/auth',
-        headers: :any,
-        methods: [:post, :options]
-      resource '/todo',
-        headers: :any,
-        methods: [:get, :post, :delete, :options]
-    end
+    # allow do
+    #   origins '*'
+    #   resource '/auth',
+    #     headers: :any,
+    #     methods: [:post, :options]
+    #   resource '/todo',
+    #     headers: :any,
+    #     methods: [:get, :post, :delete, :options]
+    # end
   end

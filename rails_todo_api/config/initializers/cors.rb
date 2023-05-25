7b79797cbf2dd929eaae13bb4ab612d2d1201259
Corbyn Greenwood <corbyn.greenwood.98@gmail.com>
2023-05-25 18:09:38 -0400

    Last attempt before I just make it public access

--- rails_todo_api/config/initializers/cors.rb
+++ rails_todo_api/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '::1', '127.0.0.1', 'rails-todo.corbyngreenwood.com', '(http[s]{0,1}\/\/){0,1}rails\-todo\.corbyngreenwood\.com\/{0,1}'
+      origins '::1', '127.0.0.1', 'rails-todo.corbyngreenwood.com', '(http[s]{0,1}:\/\/){0,1}rails\-todo\.corbyngreenwood\.com\/{0,1}'
       resource '/auth',
         headers: :any,
         methods: [:post, :options]

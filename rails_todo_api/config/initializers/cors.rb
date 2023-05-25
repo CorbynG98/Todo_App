2bd8be024e2592650379a0349e486370f37c3dda
Corbyn Greenwood <corbyn.greenwood.98@gmail.com>
2023-05-25 19:51:10 -0400

    Reverted all the weird port stuff. Added all access public host for CORS access as I need to figure out some more CORS related config with docker on GCP first.

--- rails_todo_api/config/initializers/cors.rb
+++ rails_todo_api/config/initializers/cors.rb
@@ -9,13 +9,15 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         methods: [:get, :post, :delete, :options]
     end
 
-    # allow do
-    #   origins '*'
-    #   resource '/auth',
-    #     headers: :any,
-    #     methods: [:post, :options]
-    #   resource '/todo',
-    #     headers: :any,
-    #     methods: [:get, :post, :delete, :options]
-    # end
+    # Above is still here as it was for testing. I think Docker causes this to not work correctly through GCP.
+    # More investigation required to figure this one out.
+    allow do
+      origins '*'
+      resource '/auth',
+        headers: :any,
+        methods: [:post, :options]
+      resource '/todo',
+        headers: :any,
+        methods: [:get, :post, :delete, :options]
+    end
   end

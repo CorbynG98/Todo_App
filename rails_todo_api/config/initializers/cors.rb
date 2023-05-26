4f216841a942d3cffda99d8c5e060679c7c328a6
Corbyn Greenwood <corbyn.greenwood.98@gmail.com>
2023-05-25 21:36:06 -0400

    Attempt at workflow config to add CORS to docker image

--- rails_todo_api/config/initializers/cors.rb
+++ rails_todo_api/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins ['127.0.0.1', '::1', /\A(http[s]{0,1}:\/\/){0,1}(rails\-todo\.corbyngreenwood\.com){1}(\/){0,1}\z/mi]
+      origins /\A(http[s]{0,1}:\/\/){0,1}(rails\-todo\.corbyngreenwood\.com){1}(\/){0,1}\z/mi
       resource '/auth',
         headers: :any,
         methods: [:post, :options]
@@ -8,16 +8,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         headers: :any,
         methods: [:get, :post, :delete, :options]
     end
-
-    # Above is still here as it was for testing. I think Docker causes this to not work correctly through GCP.
-    # More investigation required to figure this one out.
-    # allow do
-    #   origins '*'
-    #   resource '/auth',
-    #     headers: :any,
-    #     methods: [:post, :options]
-    #   resource '/todo',
-    #     headers: :any,
-    #     methods: [:get, :post, :delete, :options]
-    # end
   end

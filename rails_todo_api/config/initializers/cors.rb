eafa508ca0decf548f8badc550347b50eed7e8e0
Corbyn Greenwood <corbyn.greenwood.98@gmail.com>
2023-06-01 13:44:32 -0400

    Another attempt to fix random cors issues that don't want to go away :)

--- rails_todo_api/config/initializers/cors.rb
+++ rails_todo_api/config/initializers/cors.rb
@@ -14,8 +14,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # Google doesn't support CORS on cloud run apparently, as stupid as that shit is. Open API. Will learn CORS with a real cloud provider.
     allow do
       origins '*'
-      resource :any,
+      resource '*',
         headers: :any,
-        methods: [:get, :post, :delete, :options]
+        methods: :any
     end
   end

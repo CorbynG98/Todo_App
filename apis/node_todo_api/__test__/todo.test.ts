f4b5f5f142829353a9bc56295f4412e7549a80f5
Corbyn Greenwood <corbyn.greenwood.98@gmail.com>
2024-09-21 15:08:33 -0400

    Fix node tests

--- apis/node_todo_api/__test__/todo.test.ts
+++ apis/node_todo_api/__test__/todo.test.ts
@@ -17,7 +17,7 @@ beforeAll(async () => {
 var sessionToken: string = '';
 const authenticateUser = async () => {
   const response = await requestWithSupertest
-    .post('/auth/signin')
+    .post('/v1/auth/signin')
     .send({ username: 'test1', password: 'password' })
     .set('Accept', 'application/json');
   sessionToken = response.body.session_token;
@@ -27,10 +27,10 @@ const authenticateUser = async () => {
  * Tests for the get endpoint
  */
 describe('Get Todo', () => {
-  it('GET /todo with an authed user should succeed with 0 or more results', async () => {
+  it('GET /v1/todo with an authed user should succeed with 0 or more results', async () => {
     // Authenticate first
     const response = await requestWithSupertest
-      .get('/todo')
+      .get('/v1/todo')
       .set('Authorization', `${sessionToken}`)
       .expect('Content-Type', /json/)
       .expect(200);
@@ -38,17 +38,17 @@ describe('Get Todo', () => {
     expect(response.body.length).toBeGreaterThanOrEqual(0);
   });
 
-  it('GET /todo with an invalid authed user should fail', async () => {
+  it('GET /v1/todo with an invalid authed user should fail', async () => {
     // Authenticate first
     await requestWithSupertest
-      .get('/todo')
+      .get('/v1/todo')
       .set('Authorization', `totallyRandomToken`)
       .expect(403);
   });
 
-  it('GET /todo with an no user should fail with 401', async () => {
+  it('GET /v1/todo with an no user should fail with 401', async () => {
     // Authenticate first
-    await requestWithSupertest.get('/todo').expect(401);
+    await requestWithSupertest.get('/v1/todo').expect(401);
   });
 });
 
@@ -56,53 +56,53 @@ describe('Get Todo', () => {
  * Tests for the create endpoint
  */
 describe('Create Todo', () => {
-  it('POST /todo with valid data and authed user should succeed', async () => {
+  it('POST /v1/todo with valid data and authed user should succeed', async () => {
     await requestWithSupertest
-      .post('/todo')
+      .post('/v1/todo')
       .send({ title: 'Test todo 1' })
       .set('Authorization', `${sessionToken}`)
       .set('Accept', 'application/json')
       .expect(200);
   });
 
-  it('POST /todo with invalid data and authed user should fail', async () => {
+  it('POST /v1/todo with invalid data and authed user should fail', async () => {
     await requestWithSupertest
-      .post('/todo')
+      .post('/v1/todo')
       .send({ title: '' })
       .set('Authorization', `${sessionToken}`)
       .set('Accept', 'application/json')
       .expect(400);
   });
 
-  it('POST /todo with valid data and invalid authed user should fail', async () => {
+  it('POST /v1/todo with valid data and invalid authed user should fail', async () => {
     await requestWithSupertest
-      .post('/todo')
+      .post('/v1/todo')
       .send({ title: 'Test todo 1' })
       .set('Authorization', `totallyRandomToken`)
       .set('Accept', 'application/json')
       .expect(403);
   });
 
-  it('POST /todo with invalid data and invalid authed user should fail', async () => {
+  it('POST /v1/todo with invalid data and invalid authed user should fail', async () => {
     await requestWithSupertest
-      .post('/todo')
+      .post('/v1/todo')
       .send({ title: '' })
       .set('Authorization', `totallyRandomToken`)
       .set('Accept', 'application/json')
       .expect(403);
   });
 
-  it('POST /todo with valid data and no user should fail', async () => {
+  it('POST /v1/todo with valid data and no user should fail', async () => {
     await requestWithSupertest
-      .post('/todo')
+      .post('/v1/todo')
       .send({ title: 'Test todo 2' })
       .set('Accept', 'application/json')
       .expect(401);
   });
 
-  it('POST /todo with invalid data and no user should fail', async () => {
+  it('POST /v1/todo with invalid data and no user should fail', async () => {
     await requestWithSupertest
-      .post('/todo')
+      .post('/v1/todo')
       .send({ title: '' })
       .set('Accept', 'application/json')
       .expect(401);
@@ -113,40 +113,40 @@ describe('Create Todo', () => {
  * Tests for the remove endpoint
  */
 describe('Remove Todo', () => {
-  it('DELETE /todo with valid id and authed user should succeed', async () => {
+  it('DELETE /v1/todo with valid id and authed user should succeed', async () => {
     await requestWithSupertest
-      .delete('/todo/bbe060a5a2c11')
+      .delete('/v1/todo/bbe060a5a2c11')
       .set('Authorization', `${sessionToken}`)
       .expect(204);
   });
 
-  it('DELETE /todo with invalid id and authed user should fail', async () => {
+  it('DELETE /v1/todo with invalid id and authed user should fail', async () => {
     await requestWithSupertest
-      .delete('/todo/totallyInvalid')
+      .delete('/v1/todo/totallyInvalid')
       .set('Authorization', `${sessionToken}`)
       .expect(404);
   });
 
-  it('DELETE /todo with valid id and invalid authed user should succeed', async () => {
+  it('DELETE /v1/todo with valid id and invalid authed user should succeed', async () => {
     await requestWithSupertest
-      .delete('/todo/bbe060a5a2c11')
+      .delete('/v1/todo/bbe060a5a2c11')
       .set('Authorization', `totallyRandomToken`)
       .expect(403);
   });
 
-  it('DELETE /todo with invalid id and invalid authed user should fail', async () => {
+  it('DELETE /v1/todo with invalid id and invalid authed user should fail', async () => {
     await requestWithSupertest
-      .delete('/todo/totallyInvalid')
+      .delete('/v1/todo/totallyInvalid')
       .set('Authorization', `totallyRandomToken`)
       .expect(403);
   });
 
-  it('DELETE /todo with valid id and no authed user should fail', async () => {
-    await requestWithSupertest.delete('/todo/bbe060a5a2c11').expect(401);
+  it('DELETE /v1/todo with valid id and no authed user should fail', async () => {
+    await requestWithSupertest.delete('/v1/todo/bbe060a5a2c11').expect(401);
   });
 
-  it('DELETE /todo with invalid id and no authed user should fail', async () => {
-    await requestWithSupertest.delete('/todo/totallyInvalid').expect(401);
+  it('DELETE /v1/todo with invalid id and no authed user should fail', async () => {
+    await requestWithSupertest.delete('/v1/todo/totallyInvalid').expect(401);
   });
 });
 
@@ -154,43 +154,43 @@ describe('Remove Todo', () => {
  * Tests for the toggleComplete endpoint
  */
 describe('Toggle Todo Complete', () => {
-  it('POST /todo/:id/toggleComplete with valid id and authed user should succeed', async () => {
+  it('POST /v1/todo/:id/toggleComplete with valid id and authed user should succeed', async () => {
     await requestWithSupertest
-      .post('/todo/6dd1b73fe2845/toggleComplete')
+      .post('/v1/todo/6dd1b73fe2845/toggleComplete')
       .set('Authorization', `${sessionToken}`)
       .expect(204);
   });
 
-  it('POST /todo:id/toggleComplete with invalid id and authed user should fail', async () => {
+  it('POST /v1/todo:id/toggleComplete with invalid id and authed user should fail', async () => {
     await requestWithSupertest
-      .post('/todo/totallyInvalid/toggleComplete')
+      .post('/v1/todo/totallyInvalid/toggleComplete')
       .set('Authorization', `${sessionToken}`)
       .expect(404);
   });
 
-  it('POST /todo/:id/toggleComplete with valid id and invalid authed user should fail', async () => {
+  it('POST /v1/todo/:id/toggleComplete with valid id and invalid authed user should fail', async () => {
     await requestWithSupertest
-      .post('/todo/6dd1b73fe2845/toggleComplete')
+      .post('/v1/todo/6dd1b73fe2845/toggleComplete')
       .set('Authorization', `totallyRandomToken`)
       .expect(403);
   });
 
-  it('POST /todo:id/toggleComplete with invalid id and invalid authed user should fail', async () => {
+  it('POST /v1/todo:id/toggleComplete with invalid id and invalid authed user should fail', async () => {
     await requestWithSupertest
-      .post('/todo/totallyInvalid/toggleComplete')
+      .post('/v1/todo/totallyInvalid/toggleComplete')
       .set('Authorization', `totallyRandomToken`)
       .expect(403);
   });
 
-  it('POST /todo:id/toggleComplete with valid id and no authed user should fail', async () => {
+  it('POST /v1/todo:id/toggleComplete with valid id and no authed user should fail', async () => {
     await requestWithSupertest
-      .post('/todo/6dd1b73fe2845/toggleComplete')
+      .post('/v1/todo/6dd1b73fe2845/toggleComplete')
       .expect(401);
   });
 
-  it('POST /todo:id/toggleComplete with invalid id and no authed user should fail', async () => {
+  it('POST /v1/todo:id/toggleComplete with invalid id and no authed user should fail', async () => {
     await requestWithSupertest
-      .post('/todo/totallyInvalid/toggleComplete')
+      .post('/v1/todo/totallyInvalid/toggleComplete')
       .expect(401);
   });
 });
@@ -199,21 +199,21 @@ describe('Toggle Todo Complete', () => {
  * Tests for the clear completed endpoint
  */
 describe('Clear Completed', () => {
-  it('POST /todo/clearCompleted with authed user should succeed', async () => {
+  it('POST /v1/todo/clearCompleted with authed user should succeed', async () => {
     await requestWithSupertest
-      .post('/todo/clearCompleted')
+      .post('/v1/todo/clearCompleted')
       .set('Authorization', `${sessionToken}`)
       .expect(204);
   });
 
-  it('POST /todo/clearCompleted with invalid authed user should fail', async () => {
+  it('POST /v1/todo/clearCompleted with invalid authed user should fail', async () => {
     await requestWithSupertest
-      .post('/todo/clearCompleted')
+      .post('/v1/todo/clearCompleted')
       .set('Authorization', `totallyRandomToken`)
       .expect(403);
   });
 
-  it('POST /todo/clearCompleted with no authed user should fail', async () => {
-    await requestWithSupertest.post('/todo/clearCompleted').expect(401);
+  it('POST /v1/todo/clearCompleted with no authed user should fail', async () => {
+    await requestWithSupertest.post('/v1/todo/clearCompleted').expect(401);
   });
 });
--- apis/node_todo_api/__test__/user.test.ts
+++ apis/node_todo_api/__test__/user.test.ts
@@ -17,16 +17,16 @@ beforeAll(async () => {
 var sessionToken: string = '';
 const authenticateUser = async () => {
   const response = await requestWithSupertest
-    .post('/auth/signin')
+    .post('/v1/auth/signin')
     .send({ username: 'test1', password: 'password' })
     .set('Accept', 'application/json');
   sessionToken = response.body.session_token;
 };
 
 describe('User Signin', () => {
-  it('POST /auth/signin with valid data should return 200 with token', async () => {
+  it('POST /v1/auth/signin with valid data should return 200 with token', async () => {
     const response = await requestWithSupertest
-      .post('/auth/signin')
+      .post('/v1/auth/signin')
       .send({ username: 'test1', password: 'password' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
@@ -35,45 +35,45 @@ describe('User Signin', () => {
     expect(response.body).toHaveProperty('session_token');
   });
 
-  it('POST /auth/signin with valid username and invalid password should return 400', async () => {
+  it('POST /v1/auth/signin with valid username and invalid password should return 400', async () => {
     await requestWithSupertest
-      .post('/auth/signin')
+      .post('/v1/auth/signin')
       .send({ username: 'test999', password: 'totallyInvalid' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(401);
   });
 
-  it('POST /auth/signin with invalid username and valid password should return 400', async () => {
+  it('POST /v1/auth/signin with invalid username and valid password should return 400', async () => {
     await requestWithSupertest
-      .post('/auth/signin')
+      .post('/v1/auth/signin')
       .send({ username: 'test999', password: 'password' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(401);
   });
 
-  it('POST /auth/signin with no username should return 400', async () => {
+  it('POST /v1/auth/signin with no username should return 400', async () => {
     await requestWithSupertest
-      .post('/auth/signin')
+      .post('/v1/auth/signin')
       .send({ username: '', password: 'password' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(400);
   });
 
-  it('POST /auth/signin with no password should return 400', async () => {
+  it('POST /v1/auth/signin with no password should return 400', async () => {
     await requestWithSupertest
-      .post('/auth/signin')
+      .post('/v1/auth/signin')
       .send({ username: 'test1', password: '' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(400);
   });
 
-  it('POST /auth/signin with no username and no password should return 400', async () => {
+  it('POST /v1/auth/signin with no username and no password should return 400', async () => {
     await requestWithSupertest
-      .post('/auth/signin')
+      .post('/v1/auth/signin')
       .send({ username: '', password: '' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
@@ -82,9 +82,9 @@ describe('User Signin', () => {
 });
 
 describe('User Create', () => {
-  it('POST /auth/signup with valid data should return 201 with token', async () => {
+  it('POST /v1/auth/signup with valid data should return 201 with token', async () => {
     const response = await requestWithSupertest
-      .post('/auth/signup')
+      .post('/v1/auth/signup')
       .send({ username: 'testCreate', password: 'password' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
@@ -93,36 +93,36 @@ describe('User Create', () => {
     expect(response.body).toHaveProperty('session_token');
   });
 
-  it('POST /auth/signup with existing username should return 400', async () => {
+  it('POST /v1/auth/signup with existing username should return 400', async () => {
     await requestWithSupertest
-      .post('/auth/signup')
+      .post('/v1/auth/signup')
       .send({ username: 'test1', password: 'password' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(400);
   });
 
-  it('POST /auth/signup with no username should return 400', async () => {
+  it('POST /v1/auth/signup with no username should return 400', async () => {
     await requestWithSupertest
-      .post('/auth/signup')
+      .post('/v1/auth/signup')
       .send({ username: '', password: 'password' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(400);
   });
 
-  it('POST /auth/signup with no password should return 400', async () => {
+  it('POST /v1/auth/signup with no password should return 400', async () => {
     await requestWithSupertest
-      .post('/auth/signup')
+      .post('/v1/auth/signup')
       .send({ username: 'test999', password: '' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(400);
   });
 
-  it('POST /auth/signup with no username and no password should return 400', async () => {
+  it('POST /v1/auth/signup with no username and no password should return 400', async () => {
     await requestWithSupertest
-      .post('/auth/signup')
+      .post('/v1/auth/signup')
       .send({ username: '', password: '' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
@@ -131,26 +131,26 @@ describe('User Create', () => {
 });
 
 describe('User Signout', () => {
-  it('POST /auth/signout with a valid session should succeed', async () => {
+  it('POST /v1/auth/signout with a valid session should succeed', async () => {
     await requestWithSupertest
-      .post('/auth/signout')
+      .post('/v1/auth/signout')
       .set('Authorization', `${sessionToken}`)
       .set('Accept', 'application/json')
       .expect(204);
   });
 
-  it('POST /auth/signout with an invalid session should fail', async () => {
+  it('POST /v1/auth/signout with an invalid session should fail', async () => {
     await requestWithSupertest
-      .post('/auth/signout')
+      .post('/v1/auth/signout')
       .set('Accept', 'application/json')
       .set('Authorization', `superInvalidToken`)
       .expect('Content-Type', /json/)
       .expect(403);
   });
 
-  it('POST /auth/signout with no token should return 401', async () => {
+  it('POST /v1/auth/signout with no token should return 401', async () => {
     await requestWithSupertest
-      .post('/auth/signout')
+      .post('/v1/auth/signout')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(401);
